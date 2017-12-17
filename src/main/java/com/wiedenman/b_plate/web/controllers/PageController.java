4d269d94ff36332e1688dee15ea33ed33efefa20
landongw <landon@extantpublishing.com>
2017-12-17 12:18:25 -0600

    update comments

--- src/main/java/com/wiedenman/b_plate/web/controllers/PageController.java
+++ src/main/java/com/wiedenman/b_plate/web/controllers/PageController.java
@@ -15,20 +15,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.security.Principal;
 
 /**
- *   _                       _         _
- *  | |                     | |       | |
- *  | |__    ______   _ __  | |  __ _ | |_  ___
- *  | '_ \| |______| | '_ \ | | / _` || __|/ _ \
- *  | |_) |          | |_) || || (_| || |_|  __/
- *  |_.__/           | .__/ |_| \__,_| \__|\___|
- *      	         | |
- *  			     |_|
+ *    888                      888          888
+ *    888                      888          888
+ *    888                      888          888
+ *    88888b.         88888b.  888  8888b.  888888 .d88b.
+ *    888 "88b        888 "88b 888     "88b 888   d8P  Y8b
+ *    888  888 888888 888  888 888 .d888888 888   88888888
+ *    888 d88P        888 d88P 888 888  888 Y88b. Y8b.
+ *    88888P"         88888P"  888 "Y888888  "Y888 "Y8888
+ *                    888
+ *                    888
+ *                    888
+ *
  *
  * @author by Landon Wiedenman - github.com/landongw/b-plate
  *
  * License: for personal non-commercial use only.  Please contact me for commercial uses.
  *
- * */
+ */
 
 @Controller
 @RequestMapping("page")
