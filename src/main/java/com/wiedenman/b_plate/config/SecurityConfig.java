c8330c32846a52ceace99a07860cd58e7cdd0f12
landongw <landon@extantpublishing.com>
2017-12-17 11:48:58 -0600

    replace tabs with spaces in comment

--- src/main/java/com/wiedenman/b_plate/config/SecurityConfig.java
+++ src/main/java/com/wiedenman/b_plate/config/SecurityConfig.java
@@ -27,8 +27,8 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
  *  | '_ \| |______| | '_ \ | | / _` || __|/ _ \
  *  | |_) |          | |_) || || (_| || |_|  __/
  *  |_.__/           | .__/ |_| \__,_| \__|\___|
- *      	         | |
- *  			     |_|
+ *                   | |
+ *                   |_|
  *
  * @author by Landon Wiedenman - github.com/landongw/b-plate
  *
