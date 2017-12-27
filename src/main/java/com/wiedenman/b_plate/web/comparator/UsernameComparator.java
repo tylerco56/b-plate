b1fa4ef3b8366a1e714135f0396efd4bdcc56cb2
landongw <landon@extantpublishing.com>
2017-12-27 12:16:30 -0600

    override

--- src/main/java/com/wiedenman/b_plate/web/comparator/UsernameComparator.java
+++ src/main/java/com/wiedenman/b_plate/web/comparator/UsernameComparator.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 
 public class UsernameComparator implements Comparator<User> {
 
+    @Override
     public int compare(User o1, User o2) {
         return o1.getUsername().compareTo(o2.getUsername());
     }
