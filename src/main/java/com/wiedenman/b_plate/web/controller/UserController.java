eb0996b549430233dfaeab616f9c6733b787bab0
landongw <landon@extantpublishing.com>
2018-01-01 19:13:33 -0600

    add profile page, update profile page, and close my account page

--- src/main/java/com/wiedenman/b_plate/web/controller/UserController.java
+++ src/main/java/com/wiedenman/b_plate/web/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.wiedenman.b_plate.web.controller;
 
+import com.wiedenman.b_plate.dao.RoleDao;
 import com.wiedenman.b_plate.exception.EmailExistsException;
 import com.wiedenman.b_plate.exception.UsernameExistsException;
 import com.wiedenman.b_plate.service.UserService;
 import com.wiedenman.b_plate.web.comparator.UsernameComparator;
 import com.wiedenman.b_plate.web.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,6 +16,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.*;
 
 /**
@@ -43,6 +46,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RoleDao roleDao;
 
     @RequestMapping(value = "users")
     public String index(Model model) {
@@ -100,6 +106,59 @@ public class UserController {
         return "user/edit";
     }
 
+    @RequestMapping(value = "profile")
+    public String profile(Model model, Principal principal) {
+
+        User user = (User)((UsernamePasswordAuthenticationToken)principal).getPrincipal();
+        model.addAttribute("title", "MY PROFILE");
+        model.addAttribute("user", user);
+        return "user/profile";
+    }
+
+    @RequestMapping(value = "update-profile", method = RequestMethod.POST)
+    public String updateProfile(@ModelAttribute @Valid User user,
+                                final BindingResult result,
+                                Errors errors,
+                                Principal principal,
+                                Model model) {
+
+        model.addAttribute("title", "MY PROFILE");
+
+        List<ObjectError> allErrors = errors.getAllErrors();
+        ArrayList<ObjectError> filteredErrors = new ArrayList<>();
+        for (ObjectError error : allErrors) {
+            if (!error.getDefaultMessage().contains("Password")) {
+                filteredErrors.add(error);
+            }
+        }
+
+        User dbUser = (User)((UsernamePasswordAuthenticationToken)principal).getPrincipal();
+        user.setPassword(dbUser.getPassword());
+        user.setVerifyPassword(dbUser.getVerifyPassword());
+        user.setRole(roleDao.findById(dbUser.getRole().getId()));
+
+        if (filteredErrors.size() != 0) {
+            return "user/profile";
+        } try {
+            dbUser.setUsername(user.getUsername());
+            dbUser.setEmail(user.getEmail());
+            dbUser.setPhoneNumber(user.getPhoneNumber());
+            dbUser.setFirstName(user.getFirstName());
+            dbUser.setLastName(user.getLastName());
+            dbUser.setEnabled(user.isEnabled());
+            userService.save(dbUser);
+        } catch (EmailExistsException e) {
+            result.addError(new FieldError("user", "email", e.getMessage()));
+            model.addAttribute("user", user);
+            return "user/profile";
+        } catch (UsernameExistsException e) {
+            result.addError(new FieldError("user", "username", e.getMessage()));
+            model.addAttribute("user", user);
+            return "user/profile";
+        }
+        return "redirect:/profile";
+    }
+
     @RequestMapping(value = "user-disable", method = RequestMethod.POST)
     public String disableUser(@RequestParam long user_id) throws EmailExistsException, UsernameExistsException {
 
@@ -116,5 +175,15 @@ public class UserController {
         user.setEnabled(true);
         userService.save(user);
         return "redirect:users";
+    }
+
+    @RequestMapping(value = "close-my-account", method = RequestMethod.POST)
+    public String closeMyAccount(@RequestParam long user_id,
+                                 Principal principal) throws EmailExistsException, UsernameExistsException {
+
+        User user = (User)((UsernamePasswordAuthenticationToken)principal).getPrincipal();
+        user.setEnabled(false);
+        userService.save(user);
+        return "goodbye";
     }
 }
